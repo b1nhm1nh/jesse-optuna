b0b0ccecbe29cfa8b6dc951673326773b3e957b6
B1nh M1nh <binhminhbeu@gmail.com>
2022-03-09 14:43:42 +0700

    work with jesse-cli & jesse-tk

--- jesse_optuna/__init__.py
+++ jesse_optuna/__init__.py
@@ -11,13 +11,16 @@ import numpy as np
 import optuna
 import pkg_resources
 import yaml
-from jesse.research import backtest, get_candles
+# from jesse.research import backtest, get_candles
 from .JoblilbStudy import JoblibStudy
 
+import json
+from subprocess import PIPE, Popen, call
 
+from jessetk.utils import make_route, get_metrics3
 logger = logging.getLogger()
 logger.addHandler(logging.FileHandler("jesse-optuna.log", mode="w"))
-
+config_filename = 'optuna_config.yml'
 optuna.logging.enable_propagation()
 
 # create a Click group
@@ -59,34 +62,54 @@ def create_db(db_name: str) -> None:
 
 
 @cli.command()
-def run() -> None:
+@click.option(
+    '--config', default='optuna_config.yml', show_default=True,
+    help='Config file')
+def run(config : str) -> None:
+    global config_filename
+    config_filename = config
     validate_cwd()
 
     cfg = get_config()
     study_name = f"{cfg['strategy_name']}-{cfg['exchange']}-{cfg['symbol']}-{cfg['timeframe']}"
     storage = f"postgresql://{cfg['postgres_username']}:{cfg['postgres_password']}@{cfg['postgres_host']}:{cfg['postgres_port']}/{cfg['postgres_db_name']}"
 
+    make_route("route_tpl.py", "routes.py", cfg['exchange'], cfg['symbol'], cfg['timeframe'], cfg['strategy_name'])
+
     sampler = optuna.samplers.NSGAIISampler(population_size=cfg['population_size'], mutation_prob=cfg['mutation_prob'],
                                             crossover_prob=cfg['crossover_prob'], swapping_prob=cfg['swapping_prob'])
-
+    
     optuna.logging.enable_propagation()
-    optuna.logging.disable_default_handler()
+    # optuna.logging.disable_default_handler()
 
+    # try:
+    #     study = JoblibStudy(study_name=study_name, directions=["maximize", "maximize"], sampler=sampler,
+    #                                 storage=storage, load_if_exists=False)
+    # except optuna.exceptions.DuplicatedStudyError:
+    #     if click.confirm('Previous study detected. Do you want to resume?', default=True):
+    #         study = JoblibStudy(study_name=study_name, directions=["maximize", "maximize"], sampler=sampler,
+    #                                     storage=storage, load_if_exists=True)
+    #     elif click.confirm('Delete previous study and start new?', default=False):
+    #         optuna.delete_study(study_name=study_name, storage=storage)
+    #         study = JoblibStudy(study_name=study_name, directions=["maximize", "maximize"], sampler=sampler,
+    #                                     storage=storage, load_if_exists=False)
+    #     else:
+    #         print("Exiting.")
+    #         exit(1)
     try:
-        study = JoblibStudy(study_name=study_name, direction="maximize", sampler=sampler,
-                                    storage=storage, load_if_exists=False)
+        study = optuna.create_study(study_name=study_name, direction="maximize", sampler=sampler,
+                                            storage=storage, load_if_exists=False)
     except optuna.exceptions.DuplicatedStudyError:
         if click.confirm('Previous study detected. Do you want to resume?', default=True):
-            study = JoblibStudy(study_name=study_name, direction="maximize", sampler=sampler,
-                                        storage=storage, load_if_exists=True)
+            study = optuna.create_study(study_name=study_name, direction="maximize", sampler=sampler,
+                                            storage=storage, load_if_exists=True)
         elif click.confirm('Delete previous study and start new?', default=False):
             optuna.delete_study(study_name=study_name, storage=storage)
-            study = JoblibStudy(study_name=study_name, direction="maximize", sampler=sampler,
-                                        storage=storage, load_if_exists=False)
+            study = optuna.create_study(study_name=study_name, direction="maximize", sampler=sampler,
+                                            storage=storage, load_if_exists=False)
         else:
             print("Exiting.")
             exit(1)
-
     study.set_user_attr("strategy_name", cfg['strategy_name'])
     study.set_user_attr("exchange", cfg['exchange'])
     study.set_user_attr("symbol", cfg['symbol'])
@@ -95,21 +118,26 @@ def run() -> None:
 
     study.optimize(objective, n_jobs=cfg['n_jobs'], n_trials=cfg['n_trials'])
 
+    print_best_params(study)
+    save_best_params(study, study_name)
 
 def get_config():
-    cfg_file = pathlib.Path('optuna_config.yml')
+    global config_filename
+
+    cfg_file = pathlib.Path(config_filename)
 
     if not cfg_file.is_file():
-        print("optuna_config.yml not found. Run create-config command.")
+        print(f"{config_filename} not found. Run create-config command.")
         exit()
     else:
-        with open("optuna_config.yml", "r") as ymlfile:
+        with open(config_filename, "r") as ymlfile:
             cfg = yaml.load(ymlfile, yaml.SafeLoader)
 
     return cfg
 
 
 def objective(trial):
+
     cfg = get_config()
 
     StrategyClass = jh.get_strategy_class(cfg['strategy_name'])
@@ -133,16 +161,19 @@ def objective(trial):
         training_data_metrics = backtest_function(cfg['timespan-train']['start_date'],
                                                   cfg['timespan-train']['finish_date'],
                                                   trial.params, cfg)
+                                                  
     except Exception as err:
+        print("Objective error 1")
         logger.error("".join(traceback.TracebackException.from_exception(err).format()))
         raise err
 
-
     if training_data_metrics is None:
+        print("Objective error 2")
         return np.nan
 
 
-    if training_data_metrics['total'] <= 5:
+    if training_data_metrics['total'] <= 0:
+        print("Objective error 3")
         return np.nan
 
     total_effect_rate = np.log10(training_data_metrics['total']) / np.log10(cfg['optimal-total'])
@@ -176,15 +207,50 @@ def objective(trial):
         return np.nan
 
     score = total_effect_rate * ratio_normalized
+    logger.info(f"Training data metrics: {training_data_metrics}")
 
+    # print(f"Training data metrics: {training_data_metrics}")
     try:
         testing_data_metrics = backtest_function(cfg['timespan-testing']['start_date'], cfg['timespan-testing']['finish_date'], trial.params, cfg)
     except Exception as err:
         logger.error("".join(traceback.TracebackException.from_exception(err).format()))
         raise err
+    logger.info(f"Testing data metrics: {testing_data_metrics}")
 
     if testing_data_metrics is None:
         return np.nan
+
+    total_effect_rate = np.log10(testing_data_metrics['total']) / np.log10(cfg['optimal-total'])
+    total_effect_rate = min(total_effect_rate, 1)
+    ratio_config = cfg['fitness-ratio']
+    if ratio_config == 'sharpe':
+        ratio = testing_data_metrics['sharpe_ratio']
+        ratio_normalized = jh.normalize(ratio, -.5, 5)
+    elif ratio_config == 'calmar':
+        ratio = testing_data_metrics['calmar_ratio']
+        ratio_normalized = jh.normalize(ratio, -.5, 30)
+    elif ratio_config == 'sortino':
+        ratio = testing_data_metrics['sortino_ratio']
+        ratio_normalized = jh.normalize(ratio, -.5, 15)
+    elif ratio_config == 'omega':
+        ratio = testing_data_metrics['omega_ratio']
+        ratio_normalized = jh.normalize(ratio, -.5, 5)
+    elif ratio_config == 'serenity':
+        ratio = testing_data_metrics['serenity_index']
+        ratio_normalized = jh.normalize(ratio, -.5, 15)
+    elif ratio_config == 'smart sharpe':
+        ratio = testing_data_metrics['smart_sharpe']
+        ratio_normalized = jh.normalize(ratio, -.5, 5)
+    elif ratio_config == 'smart sortino':
+        ratio = testing_data_metrics['smart_sortino']
+        ratio_normalized = jh.normalize(ratio, -.5, 15)
+    else:
+        raise ValueError(
+            f'The entered ratio configuration `{ratio_config}` for the optimization is unknown. Choose between sharpe, calmar, sortino, serenity, smart shapre, smart sortino and omega.')
+    # if ratio < 0:
+    #     return np.nan
+
+    testing_score = total_effect_rate * ratio_normalized
 
     for key, value in testing_data_metrics.items():
         if isinstance(value, np.integer):
@@ -203,7 +269,8 @@ def objective(trial):
         elif isinstance(value, np.ndarray):
             value = value.tolist()
         trial.set_user_attr(f"training-{key}", value)
-    return score
+    # print(score)
+    return score, testing_score
 
 def validate_cwd() -> None:
     """
@@ -234,53 +301,38 @@ def get_candles_with_cache(exchange: str, symbol: str, start_date: str, finish_d
 
     return candles
 
+def warmup_candles_with_cache(exchange: str, symbol: str, start_date: str, finish_date: str) -> np.ndarray:
+    load_required_candles(exchange, symbol,  start_date, finish_date)
+    path = pathlib.Path('storage/jesse-optuna')
+    path.mkdir(parents=True, exist_ok=True)
+
+    cache_file_name = f"{exchange}-{symbol}-1m-{start_date}-{finish_date}.pickle"
+    cache_file = pathlib.Path(f'storage/jesse-optuna/{cache_file_name}')
+
+    if cache_file.is_file():
+        with open(f'storage/jesse-optuna/{cache_file_name}', 'rb') as handle:
+            candles = pickle.load(handle)
+    else:
+        candles = get_candles(exchange, symbol, '1m', start_date, finish_date)
+        with open(f'storage/jesse-optuna/{cache_file_name}', 'wb') as handle:
+            pickle.dump(candles, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return candles
 
 def backtest_function(start_date, finish_date, hp, cfg):
 
-    candles = {}
-    extra_routes = []
-    if len(cfg['extra_routes']) != 0:
-        for extra_route in cfg['extra_routes'].items():
-            extra_route = extra_route[1]
-            candles[jh.key(extra_route['exchange'], extra_route['symbol'])] = {
-                'exchange': extra_route['exchange'],
-                'symbol': extra_route['symbol'],
-                'candles': get_candles_with_cache(
-                    extra_route['exchange'],
-                    extra_route['symbol'],
-                    start_date,
-                    finish_date,
-                ),
-            }
-            extra_routes.append({'exchange': extra_route['exchange'], 'symbol': extra_route['symbol'],
-                                 'timeframe': extra_route['timeframe']})
-    candles[jh.key(cfg['exchange'], cfg['symbol'])] = {
-        'exchange': cfg['exchange'],
-        'symbol': cfg['symbol'],
-        'candles': get_candles_with_cache(
-            cfg['exchange'],
-            cfg['symbol'],
-            start_date,
-            finish_date,
-        ),
-    }
+    hps = json.dumps(hp)
+    # print (hps)
+    process = Popen(['jesse-tk', 'backtest', start_date,
+                    finish_date, '--hp', hps], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    output = output.decode('utf-8')
+    # logger.error(output)
+    metrics = get_metrics3(output)    
 
-    route = [{'exchange': cfg['exchange'], 'strategy': cfg['strategy_name'], 'symbol': cfg['symbol'],
-              'timeframe': cfg['timeframe']}]
-
-    config = {
-        'starting_balance': cfg['starting_balance'],
-        'fee': cfg['fee'],
-        'futures_leverage': cfg['futures_leverage'],
-        'futures_leverage_mode': cfg['futures_leverage_mode'],
-        'exchange': cfg['exchange'],
-        'settlement_currency': cfg['settlement_currency'],
-        'warm_up_candles': cfg['warm_up_candles']
-    }
-
-
-    backtest_data = backtest(config, route, extra_routes, candles, hp)['metrics']
-
+    backtest_data = json.loads(metrics['json_metrics'])
+    # print (backtest_data)
     if backtest_data['total'] == 0:
         backtest_data = {'total': 0, 'total_winning_trades': None, 'total_losing_trades': None,
                          'starting_balance': None, 'finishing_balance': None, 'win_rate': None,
@@ -297,3 +349,22 @@ def backtest_function(start_date, finish_date, hp, cfg):
                          'current_streak': None}
 
     return backtest_data
+
+def print_best_params(study):
+    print("Number of finished trials: ", len(study.trials))
+
+    trials = sorted(study.best_trials, key=lambda t: t.values)
+
+    for trial in trials:
+        print(f"Trial #{trial.number} Values: { trial.values} {trial.params}")
+
+
+def save_best_params(study, study_name: str):
+    with open("results.txt", "a") as f:
+        f.write(f"{study_name} Number of finished trials: {len(study.trials)}\n")
+
+        trials = sorted(study.best_trials, key=lambda t: t.values)
+
+        for trial in trials:
+            f.write(
+                f"Trial: {trial.number} Values: {trial.values} Params: {trial.params}\n")
